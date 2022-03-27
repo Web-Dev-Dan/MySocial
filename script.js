6fd95aaf5120fa6b7cecb38c7e197c88b31664f2
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2022-03-27 16:48:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
+// Toggle Sidebar
+const sidebar = document.querySelector('.sidebar');
+const toggleSidebarBtn = document.querySelector('.toggle-sidebar');
+const toggleSidebarIcon = document.querySelector('.toggle-sidebar-icon');
 
+toggleSidebarBtn.addEventListener('click', toggleSidebar);
+
+function toggleSidebar() {
+    sidebar.classList.toggle('sidebar-closed');
+    toggleSidebarIcon.classList.toggle('toggle-sidebar-icon-closed');
+}
+
+// Toggle Sidebar Options
+const toggleInformationBtn = document.getElementById('toggleInformationBtn');
+const toggleTemplatesBtn = document.getElementById('toggleTemplatesBtn');
+const toggleCustomisationBtn = document.getElementById('toggleCustomisationBtn');
+
+const toggleInformationBtnIcon = document.getElementById('toggleInformationBtnIcon');
+const toggleTemplatesBtnIcon = document.getElementById('toggleTemplatesBtnIcon');
+const toggleCustomisationBtnIcon = document.getElementById('toggleCustomisationBtnIcon');
+
+const informationContainer = document.querySelector('.information-container');
+const templatesContainer = document.querySelector('.templates-container');
+const customisationContainer = document.querySelector('.customisation-container');
+
+toggleInformationBtn.addEventListener('click', function () {
+    informationContainer.classList.toggle('container-hidden');
+    if (toggleInformationBtnIcon.classList.contains('fa-plus')) {
+        toggleInformationBtnIcon.classList.remove('fa-plus');
+        toggleInformationBtnIcon.classList.add('fa-minus');
+    } else {
+        toggleInformationBtnIcon.classList.add('fa-plus');
+        toggleInformationBtnIcon.classList.remove('fa-minus');
+    }
+});
+
+toggleTemplatesBtn.addEventListener('click', function () {
+    templatesContainer.classList.toggle('container-hidden');
+    if (toggleTemplatesBtnIcon.classList.contains('fa-plus')) {
+        toggleTemplatesBtnIcon.classList.remove('fa-plus');
+        toggleTemplatesBtnIcon.classList.add('fa-minus');
+    } else {
+        toggleTemplatesBtnIcon.classList.add('fa-plus');
+        toggleTemplatesBtnIcon.classList.remove('fa-minus');
+    }
+});
+
+toggleCustomisationBtn.addEventListener('click', function () {
+    customisationContainer.classList.toggle('container-hidden');
+    if (toggleCustomisationBtnIcon.classList.contains('fa-plus')) {
+        toggleCustomisationBtnIcon.classList.remove('fa-plus');
+        toggleCustomisationBtnIcon.classList.add('fa-minus');
+    } else {
+        toggleCustomisationBtnIcon.classList.add('fa-plus');
+        toggleCustomisationBtnIcon.classList.remove('fa-minus');
+    }
+});
+
+
+{/* <i class="fa-solid fa-minus"></i> */ }
+
+
+
+// Update MySocial
+function updateMySocial() {
+
+}
